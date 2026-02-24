4f91c37a81349e373cfff5607002c26a1b8f9639
Shaad <shaadahmadbh@gmail.com>
2026-02-25 01:27:34 +0530

    Add files via upload

---
+++ conditions.js
@@ -0,0 +1,9 @@
+a=100,b=290;
+if(a>b){
+    console.log(a);
+}
+else{
+    console.log(b);
+}
+
+(10>90) ? console.log("S1"):console.log("S2");
---
+++ operators.js
@@ -0,0 +1,15 @@
+a=2;
+console.log(++a);
+console.log(a);
+console.log(a);
+
+b=30;
+b+=31;
+console.log(b);
+/*valid for all operations)*/
+
+a=4,b=10;
+console.log(a<=b);
+
+a=1,b=10;
+console.log(a>=b);
