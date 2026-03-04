6d9d15a1a16e188605ba4072a19d7a45b552b064
Shaad <shaadahmadbh@gmail.com>
2026-03-04 21:19:45 +0530

    Add files via upload

---
+++ array2.js
@@ -0,0 +1,17 @@
+let a=[44,48,24];
+for (i=0;i<3;i=i+1){
+    console.log(a[i]);
+}
+
+for (p of a){
+    console.log(p);
+}
+for(p in a){
+    console.log(a[p]);
+}
+for(p in a){
+    console.log(a);
+}
+for(p in a){
+    console.log(p);
+}
---
+++ array3.js
@@ -0,0 +1,8 @@
+let a=[44,48,24];
+for (i=0;i<3;i=i+1){
+    console.log(a[i]);
+}
+b=["secundrabad",'metpally',`panjagutta`];
+for (i=0;i<3;i=i+1){
+    console.log(b[i].toUpperCase());
+}
